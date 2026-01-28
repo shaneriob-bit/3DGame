876a96d53e97a484ddb029997d324111e94e7e8c
shaneriob-bit <shaneriob@gmail.com>
2026-01-28 09:15:57 -0500

    object pooling for debris

--- Assets/Scripts/DebrisManager.cs
+++ Assets/Scripts/DebrisManager.cs
@@ -6,6 +6,8 @@ public class DebrisManager : MonoBehaviour
     public GameObject debrisPrefab;
     public float squareSize = 8.0f;
     public int debrisListSize = 1000;
+    public int maxClusterSize = 16;
+    public float maxScaleMultiplier = 3.0f;
     private List<GameObject> debrisList = new List<GameObject>();
 
     int usedCount = 0;
@@ -34,31 +36,13 @@ public class DebrisManager : MonoBehaviour
         }
         return activeCount;
     }
-
-    public void ResetDebris(int index)
+        public void ResetDebris(int index)
     {
         debrisList[index].transform.position = new Vector3(Random.Range(-squareSize, squareSize), transform.position.y, Random.Range(-squareSize, squareSize));
+        debrisList[index].transform.localScale = debrisPrefab.transform.localScale * Random.Range(1.0f, maxScaleMultiplier);
+        debrisList[index].GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         debrisList[index].SetActive(true);
     }
-
-    /*
-    public void RainDebris(int count)
-    {
-        for(int i = 0; i < count; i++)
-        {
-            if(usedCount <= debrisList.Count)
-            {
-                Debug.Log(usedCount);
-                ResetDebris(usedCount);
-                usedCount++;
-            }
-            if(usedCount >= debrisList.Count)
-            {
-                usedCount = 0;
-            }
-        }
-    }
-    */
 
     public void RainDebris(int count)
     {
@@ -80,10 +64,18 @@ public class DebrisManager : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for(int i = 0; i < debrisList.Count; i++)
+        {
+            if(Vector3.Distance(debrisList[i].transform.position, Vector3.zero) > 9999.0f)
+            {
+                debrisList[i].SetActive(false);
+            }
+        }
+
 
         if(Random.Range(0, 20) == 17)
         {
-            RainDebris(Random.Range(1, 30)); //CreateDebris(Random.Range(1, 30));
+            RainDebris(Random.Range(1, maxClusterSize)); //CreateDebris(Random.Range(1, 30));
         }
     }
 }
